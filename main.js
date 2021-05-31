a221af9d48762fbf28b600d74a4f8f593132f062
the1coder-pro <mohamed2015pre@gmail.com>
2021-05-31 13:32:41 +0300

    adding features

--- main.js
+++ main.js
@@ -1,16 +1,58 @@
 const express = require('express')
+const path = require('path')
+const { body, validationResult } = require('express-validator');
+
+
+// Init App
 const app = express()
+
+// Load View Engine
+app.use('/static', express.static(path.join(__dirname, 'views')))
+app.set('view engine', 'pug')
+
+// const MONTHSOptions = { "January": 1, "February": 2, "March": 3, "April": 4, "May": 5, "June": 6, "July": 7, "August": 8, "September": 9, "October": 10, "November": 11, "December": 12 };
+
+// Home Route
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
 app.listen(port, () => {
-    console.log(`Example app listen at http://localhost:${port}`)
+    console.log(`Server started at http://localhost:${port}`)
 })
 
-app.set('view engine', 'pug')
-app.use(express.static('public'))
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.post('/result', (req, res) => {
+    const inputRequest = req.body
+    var showResponseMessage = false
+    var ResponseMessage
+    if (inputRequest.inputYear == '') {
+        showResponseMessage = true
+        ResponseMessage = "missing Year"
+    } else if (inputRequest.inputDay == '') {
+        showResponseMessage = true
+        ResponseMessage = "missing Day"
+
+    } else if (inputRequest.inputDay > '28' && inputRequest.inputMonth == '2') {
+        showResponseMessage = true
+        ResponseMessage = "wrong day"
+
+    } else {
+        showResponseMessage = false
+        ResponseMessage = "Thank you"
+    }
+
+
+    console.log(inputRequest)
+    res.render('index', {
+        showMessage: showResponseMessage,
+        theMessage: ResponseMessage
+    })
+})
 
 
