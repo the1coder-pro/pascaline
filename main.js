b7989fbc31d3727ba5276c26124c6769a4ee2f12
the1coder-pro <mohamed2015pre@gmail.com>
2021-06-01 18:06:36 +0300

    Adding Styles and other stuff

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
-const { body, validationResult } = require('express-validator');
+var ageFinder = require('./views/age')
+// const { body, validationResult } = require('express-validator');
 
 
 // Init App
@@ -39,9 +40,45 @@ app.use(express.urlencoded({
     extended: true
 }))
 
+var numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+}
 
-app.get('/:year/:month/:day', (req, res) => {
-    res.send(req.params)
+app.post('/:year/:month/:day', (req, res) => {
+    var requestParams = req.params;
+    let showTimes = false;
+
+    let ts = Date.now();
+    let date_ob = new Date(ts);
+    var current_date = date_ob.getDate();
+    var current_month = date_ob.getMonth() + 1;
+    var current_year = date_ob.getFullYear();
+
+    if (requestParams.year != "" && requestParams.month != "" && requestParams.day != "") {
+        var [calculated_date, calculated_month, calculated_year, current_date, current_month, current_year, birthDay_date, today_date, days, YearWithMonths, YearsToMonths, restOfMonths, MonthsToWeeks, restOfWeeks, WeeksToDays, DaysToHours, HoursToMinutes, MinutesToSeconds, SecondsToMilliseconds, MillisecondsToMicroSeconds, MicroSecondsToNanoSeconds] = ageFinder.find_age(current_date, current_month, current_year, requestParams.day, requestParams.month, requestParams.year);
+        showTimes = true;
+        res.render('index', {
+            showTimes: showTimes,
+            age_years: calculated_year,
+            age_months: calculated_month,
+            age_days: calculated_date,
+            yearsToMonths: YearsToMonths,
+            restOfMonths: restOfMonths,
+            monthsToWeeks: MonthsToWeeks,
+            restOfWeeks: restOfWeeks,
+            ageInDays: numberWithCommas(WeeksToDays),
+            hours: numberWithCommas(DaysToHours),
+            minutes: numberWithCommas(HoursToMinutes),
+            seconds: numberWithCommas(MinutesToSeconds),
+            milliseconds: numberWithCommas(SecondsToMilliseconds),
+            microseconds: numberWithCommas(MillisecondsToMicroSeconds),
+            nanoseconds: numberWithCommas(MicroSecondsToNanoSeconds)
+
+        })
+    } else {
+        res.render('index')
+    }
+
 })
 
 // app.post('/', (req, res) => {
