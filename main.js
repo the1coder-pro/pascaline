43325b029eb6b0cdabe129adc8dbfe03be6989e9
the1coder-pro <mohamed2015pre@gmail.com>
2021-06-05 18:23:06 +0300

    Fixing Problems

--- main.js
+++ main.js
@@ -6,12 +6,15 @@ const path = require('path')
 const app = express()
 
 // Load View Engine
-app.use('/static', express.static(path.join(__dirname, 'views')))
-app.set('view engine', 'pug')
+// app.use('/static', express.static(path.join(__dirname, 'views')))
+// app.set('view engine', 'pug')
+
+// app.use(express.static('public'))
+
 
 // Home Route
 app.get('/', (req, res) => {
-    res.render('index')
+    res.sendFile('home.html', { root: path.join(__dirname, './views') });
 })
 
 var PORT = process.env.PORT || 5000
