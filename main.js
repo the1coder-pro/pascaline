419bfb02ccc48668dc8e70f0363f122f63dcce69
MutawaPro <42838648+the1coder-pro@users.noreply.github.com>
2021-06-24 07:43:21 +0300

    Cleaning main.js file

--- main.js
+++ main.js
@@ -5,13 +5,6 @@ const path = require('path')
 // Init App
 const app = express()
 
-// Load View Engine
-// app.use('/static', express.static(path.join(__dirname, 'views')))
-// app.set('view engine', 'pug')
-
-// app.use(express.static('public'))
-
-
 // Home Route
 app.get('/', (req, res) => {
     res.sendFile('home.html', { root: path.join(__dirname, './views') });
